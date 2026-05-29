d354f755c58b2bf7ee33a1589f2d807315e2ce0e
Its-jin0124 <itsakaguchi1125@gmail.com>
2026-05-29 16:46:17 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const GAS_URL = "★あなたのGASのexec URLを入れる★";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxs146Kn697zN4MccHlZrsmL0scu6KIbmivQYsunLXKH0T7jBwp6KO1c17IL2dTGFP0/exec";
 
 // 全角70文字ごとに改行する関数
 function wrapText(text, maxLen = 70) {
